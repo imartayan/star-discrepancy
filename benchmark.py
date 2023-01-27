8dbf633776cc277fc2debe8aca14b6a28e94cd98
Igor Martayan <igor@martayan.org>
2023-01-27 19:12:53 +0100

    Update benchmarks, move algos to benchmark

--- benchmark.py
+++ benchmark.py
@@ -5,8 +5,39 @@ import heuristics
 from statistics import mean
 
 
+algos = {
+    "RandomSearch": heuristics.RandomSearch(),
+    "BruteForce": heuristics.BruteForce(),
+    "GaussianLS (0.3)": heuristics.GaussianLocalSearch(0.3),
+    "GaussianLS (0.2)": heuristics.GaussianLocalSearch(0.2),
+    "GaussianLS (0.1)": heuristics.GaussianLocalSearch(0.1),
+    "GaussianLS (0.05)": heuristics.GaussianLocalSearch(0.05),
+    "GaussianLS (0.03)": heuristics.GaussianLocalSearch(0.03),
+    "GaussianLS (0.01)": heuristics.GaussianLocalSearch(0.01),
+    # "GaussianSingleAxis  (0.3)": heuristics.GaussianSingleAxis(0.3),
+    # "GaussianSingleAxis  (0.2)": heuristics.GaussianSingleAxis(0.2),
+    # "GaussianSingleAxis  (0.1)": heuristics.GaussianSingleAxis(0.1),
+    # "GaussianSingleAxis  (0.05)": heuristics.GaussianSingleAxis(0.05),
+    "ExpLS (0.3)": heuristics.ExpLocalSearch(0.3),
+    "ExpLS (0.2)": heuristics.ExpLocalSearch(0.2),
+    "ExpLS (0.1)": heuristics.ExpLocalSearch(0.1),
+    "ExpLS (0.05)": heuristics.ExpLocalSearch(0.05),
+    "ExpLS (0.03)": heuristics.ExpLocalSearch(0.03),
+    "ExpLS (0.01)": heuristics.ExpLocalSearch(0.01),
+    "GaussianReset (0.3)": heuristics.GaussianReset(0.3),
+    "GaussianReset (0.2)": heuristics.GaussianReset(0.2),
+    "GaussianReset (0.1)": heuristics.GaussianReset(0.1),
+    "GaussianReset (0.05)": heuristics.GaussianReset(0.05),
+    "GaussianReset (0.03)": heuristics.GaussianReset(0.03),
+    "GaussianReset (0.01)": heuristics.GaussianReset(0.01),
+    # "GaussianCrossover  (0.1)": heuristics.GaussianCrossover(0.1),
+    # "GaussianCrossover  (0.05)": heuristics.GaussianCrossover(0.05),
+    # "GaussianCrossover  (0.03)": heuristics.GaussianCrossover(0.03),
+}
+
+
 def benchmark(fid: int, dim: int):
-    for name, algo in heuristics.all.items():
+    for name, algo in algos.items():
         means = []
         bests = []
         for iid in range(5):
@@ -22,12 +53,12 @@ def benchmark(fid: int, dim: int):
                 func.reset()
             means.append(mean(res))
             bests.append(max(res))
-        print(f"{name}\t{'%.2e' % mean(means)}")
+        print(f"{name}\t{'%.3e' % mean(means)}")
 
 
 if __name__ == "__main__":
     warnings.filterwarnings("ignore", category=RuntimeWarning)
     warnings.filterwarnings("ignore", category=FutureWarning)
-    fid = 39
-    dim = 2
+    fid = 30
+    dim = 3
     benchmark(fid, dim)
